9188f1e2392d30fa64a83f254cc4dfa4df5fc5c1
Marlon da Cunha <marloncunhaofficial@gmail.com>
2023-08-13 13:29:44 -0300

    novas atulizaçoes nas configuraçoes sass e gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-function testeGulp(cd) {
-    console.log("Olá mundo")
-    cd(); 
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = testeGulp;
+exports.default = styles;
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
